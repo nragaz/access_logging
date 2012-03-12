a1af7d42a8e4a8dae7f9d19c5932fd1ecb5b9cd9
Nick Ragaz <nick.ragaz@gmail.com>
2012-03-11 21:55:28 -0400

    dont log long post params in test

--- lib/access_logging/model.rb
+++ lib/access_logging/model.rb
@@ -2,46 +2,52 @@ require 'active_support/concern'
 
 module AccessLogging::Model
   extend ActiveSupport::Concern
-  
+
   include DateRangeScopes
-  
+
   REDIS_ATTRIBUTES = [
     :created_at,
-    :ip, :user_type, :user_id, 
+    :ip, :user_type, :user_id,
     :verb, :model_type, :model_id, :description,
     :path
   ]
-  
+
   REDIS_SEPARATOR = "||"
-  
+
   included do
     belongs_to :user, polymorphic: true
     belongs_to :model, polymorphic: true
-    
+
     before_validation :set_user_name_and_email, :set_description
-    
+
     validates_presence_of :ip, :path, :verb,
                           :user_name, :user_email, :user_id, :user_type,
                           :created_at
-    
+
     default_scope order('access_logs.created_at DESC')
   end
-  
+
   module ClassMethods
     def log_request(user, request, opts={})
       log = AccessLog.new
       log.user = user
-      log.path = request.fullpath
       log.ip = request.remote_ip
-      
+
+      if [:get, :delete].include?(request.method_symbol)
+        log.path = request.fullpath
+      else
+        # Don't log params on POST or PUT requests
+        log.path = request.fullpath.split("?")[0]
+      end
+
       log.model = opts[:model]
       log.verb = opts[:verb] || default_verb_for_method(request.request_method)
-      
+
       return if log.model && log.model.new_record?
-      
+
       log.save!
     end
-  
+
     def default_verb_for_method(method)
       case method.to_s.upcase
       when 'GET'
@@ -54,33 +60,33 @@ module AccessLogging::Model
         "deleted"
       end
     end
-  
+
     def build_from_redis_string(str)
       log = AccessLog.new
       parts = str.split(REDIS_SEPARATOR, -1)
-    
+
       REDIS_ATTRIBUTES.each_with_index do |attribute, i|
         log.send "#{attribute}=", parts[i]
       end
-    
+
       log
     end
   end
-  
+
   def created_at
     self[:created_at] ||= Time.zone.now
   end
-  
+
   def redis_string
     REDIS_ATTRIBUTES.map { |attribute| send(attribute) }.join(REDIS_SEPARATOR)
   end
-  
-  
+
+
   private
-  
+
   def set_description
     self.description = "" and return unless model && !model.new_record?
-    
+
     if model.respond_to?(:description)
       self.description = model.description
     elsif model.respond_to?(:name)
@@ -89,7 +95,7 @@ module AccessLogging::Model
       self.description = "#{model.class.name} #{model.id}"
     end
   end
-  
+
   def set_user_name_and_email
     self.user_name = user.try :name
     self.user_email = user.try :email
